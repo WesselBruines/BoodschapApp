b55be68e3263d289821fbddd4ba6505571d8d2e6
Anne-boom <anne.Boomen@gmail.com>
2020-09-18 16:09:34 +0200

    version 1

---
+++ IngredientDB/DBingredient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngredientDB
+{
+    public class DBingredient : DbContext
+    {
+
+        
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
---
+++ IngredientDB/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngredientDB
+{
+    public class Ingredient
+    {
+        [Key]
+        public int ingredientID { get; set; }
+      
+        public string name { get; set; }
+        
+        public string merk { get; set; }
+    }
+}
